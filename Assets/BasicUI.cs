b6b90198dba943cf69ca3d7c25bbc6c106234745
woneill81 <woneill1@my.hpu.edu>
2019-04-06 10:59:52 -1000

    scripts

---
+++ Assets/BasicUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicUI : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        int posX = 10;
+        int posY = 10;
+        int width = 100;
+        int height = 30;
+        int buffer = 10;
+
+        List<string> itemList = Managers.Inventory.GetItemList();
+        if (itemList.Count == 0)
+        {
+            GUI.Box(new Rect(posX, posY, width, height), "No items");
+        }
+        foreach(string items in itemList)
+        {
+            int count = Managers.Inventory.GetItemCount(items);
+            Texture2D image = Resources.Load<Texture2D>("Icons/" + items);
+            GUI.Box(new Rect(posX, posY, width, height), new GUIContent("(" + count + ")", image));
+                
+            posX += width + buffer;
+        }
+
+        string equipped = Managers.Inventory.equippedItem;
+        if (equipped != null)
+        {
+            posX = Screen.width - (width + buffer);
+            Texture2D image = Resources.Load("Icons/" + equipped) as Texture2D;
+            GUI.Box(new Rect(posX, posY, width, height), new GUIContent("Equipped", image));
+        }
+        posX = 10;
+        posY += height + buffer;
+        foreach(string item in itemList)
+        {
+            if(GUI.Button(new Rect(posX, posY, width, height), "Equip " + item))
+            {
+                Managers.Inventory.EquipItem(item);
+            }
+            if (item == "health")
+            {
+                if (GUI.Button(new Rect(posX, (posY+height+buffer), width, height), "Use Health"))
+                {
+                    Managers.Inventory.ConsumeItem("health");
+                    Managers.Player.ChangeHealth(25);
+                }
+            }
+            posX += width + buffer;
+        }
+
+    }
+}
---
+++ Assets/OrbitCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float rotspeed = 1.5f;
+    private float _rotY;
+    private Vector3 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _offset = target.position - transform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        if (horInput != 0)
+        {
+            _rotY += horInput * rotspeed;
+        }
+        else
+        {
+            _rotY += Input.GetAxis("Mouse X") * rotspeed * 3;        
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+}
