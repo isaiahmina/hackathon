b5d0dae72d509481251d04de1a8a1d9326853732
isaiahmina <isaiahkgm@gmail.com>
2019-04-06 17:19:04 -1000

    new movement script

---
+++ Assets/Scripts/Misc/blobMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blobMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter player;
+
+    public float speed = 2.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        /*if(inside the '"don't move'" volume)
+         * return
+         * */
+         Vector2 dir = new Vector2(player.transform.position.x, player.transform.position.y);
+
+    }
+}
